52ca484445b2ed33b75a58c9a032a5203be79ba6
manikamg <36034033+manikamg@users.noreply.github.com>
2024-07-10 22:07:08 +0530

    Delete backend directory

--- backend/src/routes/AuthRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-
-router.get("/", (req, res) => {
-    res.json({msg: "hello world"})
-})
-
-module.exports = router
--- backend/src/utils/CachAsnc.js
+++
@@ -1,8 +0,0 @@
-const CatchAsync = (fn) => (req, res, next) => {
-    return Promise.resolve(fn(req, res, next)).catch((e) => {
-        console.log("Promise can braked")
-        next(e)        
-    })
-}
-
-module.exports = CatchAsync
