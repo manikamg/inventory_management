30e06d65f8ab07d744050ec5a68d5127cecdecef
manikamg <36034033+manikamg@users.noreply.github.com>
2024-07-10 22:06:32 +0530

    Delete frontend directory

--- frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-// css
-import "./assets/css/main.css"
-
-const root = document.getElementById("root");
-ReactDOM.createRoot(root).render(<><App /></>);
